430859c6813afdc15ca8429749d8bf8b47156dbc
Zahid <thezaza102@gmail.com>
2023-01-24 16:47:11 +1100

    Update findFace.py

--- Tutes/OpenCV/Face/findFace.py
+++ Tutes/OpenCV/Face/findFace.py
@@ -6,33 +6,31 @@ camera = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-def detectFaces(rgbImg):
-    gray = cv2.cvtColor(rgbImg, cv2.COLOR_RGB2GRAY)
+def detectFaces(bgrImg):
+    gray = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2GRAY)
     return face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)    
 
-def drawFaceBB(rgbImg, faces):
+def drawFaceBB(bgrImg, faces):
     for (x, y, w, h) in faces:
-        cv2.rectangle(rgbImg, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    return rgbImg
+        cv2.rectangle(bgrImg, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    return bgrImg
 
 def getFrame():
     return_value, image = camera.read()
-    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
     
 
 def displayIm(img):   
-    plt.clf()
-    plt.imshow(img)  
-    plt.pause(.05)
+    cv2.imshow("Tracking", img)
 
 if __name__ == "__main__":
     while True:
-        try:
-            image = cv2.resize(getFrame(),None,fx=0.5,fy=0.5)
-            faces = detectFaces(image)
-            output = drawFaceBB(image,faces)
-            displayIm(output)
-        except:
-            pass
+        image = cv2.resize(getFrame(),None,fx=0.5,fy=0.5)
+        faces = detectFaces(image)
+        output = drawFaceBB(image,faces)
+        displayIm(output)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
     camera.release()
